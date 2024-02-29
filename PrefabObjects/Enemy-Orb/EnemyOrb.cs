c2f50a6d708dddb674c3041e0dcdd62044eff2ad
@Lairdom <kuraime@hotmail.com>
2024-02-29 14:59:33 +0200

    Enemy bullet adjustments

--- PrefabObjects/Enemy-Orb/EnemyOrb.cs
+++ PrefabObjects/Enemy-Orb/EnemyOrb.cs
@@ -7,17 +7,22 @@ public partial class EnemyOrb : CharacterBody3D
 	Node3D player = default;
 	Vector3 playerDirection;
 	float playerDistance;
-	PackedScene bulletScene = ResourceLoader.Load("res://PrefabObjects/Enemy-Orb/Bullet1.tscn") as PackedScene;
+	PackedScene bulletScene = ResourceLoader.Load("res://PrefabObjects/Enemy-Orb/Bullet.tscn") as PackedScene;
 	float shootTimer = 0;
+	bool shooting = false;
+	Node3D root = default;
 
 	void SpawnBullet() {
 		Debug.Print("Spawn Bullet");
-		Node bulletInstance = bulletScene.Instantiate();
-		AddChild(bulletInstance);
+		Node3D bulletInstance = (Node3D) bulletScene.Instantiate();
+		bulletInstance.Position = Position;
+		bulletInstance.Rotation = Rotation;
+		root.AddChild(bulletInstance);
 	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
+		root = GetNode<Node3D>("/root/World");
 		player = GetNodeOrNull<Node3D>("/root/World/Player");
 		if (player != null) {
 			Debug.Print("Player detected");
@@ -31,12 +36,16 @@ public partial class EnemyOrb : CharacterBody3D
 			playerDirection = player.GlobalPosition - GlobalPosition;
 			playerDirection = playerDirection.Normalized();
 			playerDistance = GlobalPosition.DistanceTo(player.GlobalPosition);
+			
 			LookAt(player.GlobalPosition);
 			if (playerDistance < 10 && shootTimer <= 0) {
+				shooting = true;
 				SpawnBullet();
 				shootTimer = 2;
 			}
 			shootTimer -= delta;
+			if (shootTimer <= 0)
+				shooting = false;
 		}
 	}
 }
