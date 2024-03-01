bd10fa03f609ea1874fa3c3cf22f9514adf27924
Gaabinator <158502112+Gaabinator@users.noreply.github.com>
2024-03-01 12:32:41 +0200

    Start_menu update
    If you start scene from start menu.tscn it works now

--- Scripts/StartHelpExitMenu.cs
+++ Scripts/StartHelpExitMenu.cs
@@ -3,33 +3,34 @@ using System;
 
 public partial class StartHelpExitMenu : CanvasLayer {
 
-    [ExportGroup("Buttons")]
-    [ExportSubgroup("Button Set")]
-    [Export] public Button start;
-    [Export] public Button help;
-    [Export] public Button exit;
+	[ExportGroup("Buttons")]
+	[ExportSubgroup("Button Set")]
+	[Export] public Button start;
+	[Export] public Button help;
+	[Export] public Button exit;
 
-    public override void _Ready() {
-        start.Connect("pressed", new Callable(this, nameof(OnStartPressed)));
-        help.Connect("pressed", new Callable(this, nameof(OnHelpPressed)));
-        exit.Connect("pressed", new Callable(this, nameof(OnExitPressed)));
-    }
+	public override void _Ready() {
+		start.Connect("pressed", new Callable(this, nameof(OnStartPressed)));
+		help.Connect("pressed", new Callable(this, nameof(OnHelpPressed)));
+		exit.Connect("pressed", new Callable(this, nameof(OnExitPressed)));
+	}
 
-    private void OnStartPressed() {
-        // Starts the game
-        GetTree().CallDeferred("ChangeScene", "res://Scenes/world.tscn");
-    }
+	private void OnStartPressed() {
+		// Starts the game
+		var nextScene = (PackedScene)ResourceLoader.Load("res://Scenes/world.tscn");
+		GetTree().ChangeSceneToPacked(nextScene);
+	}
 
-    private void OnHelpPressed() {
-        // Opens help -window
-    }
+	private void OnHelpPressed() {
+		// Opens help -window
+	}
 
-    private void OnExitPressed() {
-        // Closes the game
-        GetTree().Quit();
-    }
-    
-    public override void _Process(double delta)
-    {
-    }
+	private void OnExitPressed() {
+		// Closes the game
+		GetTree().Quit();
+	}
+	
+	public override void _Process(double delta)
+	{
+	}
 }
