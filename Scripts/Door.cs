eee07af7e784dd13f44e4d3f4a1acb5c2bb900a1
Gaabinator <158502112+Gaabinator@users.noreply.github.com>
2024-03-08 09:41:28 +0200

    World, doors open
    Now player can open doors pressing E key, there is still some bug in the animation process. Needs to be solved

---
+++ Scripts/Door.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class Door : Node3D
+{
+	
+	private AnimationPlayer animationPlayer;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsActionJustPressed("Examine"))
+		{
+			// Toggle the door animation
+			if (!animationPlayer.IsPlaying())
+			{
+				animationPlayer.Play("open");
+			}
+			else
+			{
+				animationPlayer.Stop();
+				animationPlayer.Play("close");
+			}
+		}
+	}
+}
